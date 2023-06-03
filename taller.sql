a8843848f0a32f5db4203264e733106d11610a7e
Aldair0117 <107733375+Aldair0117@users.noreply.github.com>
2023-06-02 23:13:24 -0600

    Practica 9
    Ejercicios corregidos, tenia errores.

--- taller.sql
+++ taller.sql
@@ -650,45 +650,48 @@ FROM my_employee;
 
  --- Practica 9
  
--- Cree una tabla llamada DEPTOS con la siguiente estructura:
+-- 1. Cree una tabla llamada DEPTOS con la siguiente estructura:
 
 CREATE TABLE DEPTOS(
  ID NUMBER(7) PRIMARY KEY,
  NAME VARCHAR2(25) NOT NULL
  );
 
--- Rellene la tabla DEPTOS con todas las filas de la tabla DEPARTMENTS. Incluya solo las columnas que necesite.
+-- 2. Rellene la tabla DEPTOS con todas las filas de la tabla DEPARTMENTS. Incluya solo las columnas que necesite.
 
-INSERT INTO DEPTOS 
-VALUES (1, ITPRO); 
+INSERT INTO DEPTOS(ID, NAME) 
+VALUES (1, 'ITPRO'); 
 
--- Cree una tabla llamada EMPS con la siguiente estructura:
+
+-- 3. Cree una tabla llamada EMPS con la siguiente estructura:
 
 CREATE TABLE EMPS(
  ID NUMBER(7) PRIMARY KEY,
  LAST_NAME VARCHAR2(25) NOT NULL,
  FIRST_NAME VARCHAR2(25) NOT NULL,
- DEPT_ID NUMBER(7) FOREIGN KEY (DEPTOS)
- REFERENCES DEPTOS(ID) 
+ DEPT_ID NUMBER(7), 
+ FOREIGN KEY (DEPT_ID) REFERENCES DEPTOS(ID) 
  );
 
--- Modifique la tabla EMPS para permitir una longitud de apellido de 35.
+-- 4. Modifique la tabla EMPS para permitir una longitud de apellido de 35.
 
 ALTER TABLE EMPS
 MODIFY (LAST_NAME VARCHAR2(35));
 
--- Agregue 3 filas cualesquiera en la tabla EMPS y consulte los datos.
+-- 5. Agregue 3 filas cualesquiera en la tabla EMPS y consulte los datos.
 
-INSERT INTO EMPS
-VALUES (1, CASTILLO , ALDAIR, 1);
-VALUES (2, VAZQUEZ , NOEL, 1);
-VALUES (3, CORTEZ , HECOAL, 1);
+INSERT INTO EMPS(ID, LAST_NAME, FIRST_NAME, DEPT_ID)
+VALUES (1, 'CASTILLO' , 'ALDAIR', 1);
+INSERT INTO EMPS(ID, LAST_NAME, FIRST_NAME, DEPT_ID)
+VALUES (2, 'VAZQUEZ' , 'NOEL', 1);
+INSERT INTO EMPS(ID, LAST_NAME, FIRST_NAME, DEPT_ID)
+VALUES (3,'CORTEZ' , 'HECOAL', 1);
 
--- Cambie el nombre de la tabla EMPS a EMPLEADOS
+-- 6. Cambie el nombre de la tabla EMPS a EMPLEADOS
 
 RENAME EMPS TO EMPLEADOS
 
--- Cree la tabla EMPLOYEES_2 basándose en la estructura de la tabla EMPLOYEES. 
+-- 7. Cree la tabla EMPLOYEES_2 basándose en la estructura de la tabla EMPLOYEES. 
 -- Incluya solo las columnas EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY y DEPARTMENT_ID. 
 -- Llame a las columnas de la nueva tabla ID, NOMBRE, APELLIDO, SALARIO y CLAVE_DEPT, respectivamente.
 
@@ -697,10 +700,10 @@ AS
     SELECT EMPLOYEE_ID AS ID, FIRST_NAME AS NOMBRE, LAST_NAME AS APELLIDO, SALARY AS SALARIO, DEPARTMENT_ID AS CLAVE_DEPT
 FROM EMPLOYEES;
 
--- Borre la columna FIRST_NAME de la tabla EMPLEADOS. 
+-- 8. Borre la columna FIRST_NAME de la tabla EMPLEADOS. 
 -- Confirme la modificación comprobando la descripción de la tabla con el comando DESC.
 
 ALTER TABLE EMPLEADOS 
-DROP FIRST_NAME;
+DROP COLUMN FIRST_NAME;
 
 DESC EMPLEADOS;
